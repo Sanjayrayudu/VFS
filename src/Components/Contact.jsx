5c2ac137c5fcbd15fdf27dd43093755e0d2f7b90
Sanjay Rayudu <93073020+Sanjayrayudu@users.noreply.github.com>
2023-06-27 16:56:23 +0530

    Update Contact.jsx

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -305,7 +305,7 @@ const Contact = () => {
              <p><i className="fas fa-home mr-3"></i> 8-3, Road No-6, Vakalpudi ,Kakinada </p>
              <p><i className="fas fa-envelope mr-3"></i> vfseafoods@gmail.com</p>
              <p><i className="fas fa-phone mr-3"></i> + 91 8790432054</p>
-             <p><i className="fas fa-phone mr-3"></i> + 91 7995236091 (Marketing : Mahesh[Hyderabad])</p>
+              <p><i className="fas fa-phone mr-3"></i> + 91 7995236091 ( Marketing : Mahesh[HYD] )</p>
            </div>
            
          </div>
