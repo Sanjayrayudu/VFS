da5cf61a384b06831e5e3ae8cf14fffc268a0f99
Sanjay Rayudu <93073020+Sanjayrayudu@users.noreply.github.com>
2024-04-08 23:25:47 +0530

    Update About.jsx

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -320,8 +320,7 @@ Fish - tilapia, pangus basa & Vietnam white basa fillet also available
               <p><i className="fas fa-home mr-3"></i> 8-3, Road No-6, Vakalpudi ,Kakinada </p>
               <p><i className="fas fa-envelope mr-3"></i> vfseafoods@gmail.com</p>
                <h6 className="text-uppercase fw-bold">Marketing</h6>
-              <p><i className="fas fa-phone mr-3"></i> + 91 9912907822 (Mahesh[Hyderabad] )</p>
-              <p><i className="fas fa-phone mr-3"></i> + 91 7995236091 (Rajesh[Delhi&Mumbai] )</p>
+              <p><i className="fas fa-phone mr-3"></i> + 91 9182261007 (A Prasanna[Hyderabad] )</p>
             </div>
             
           </div>
